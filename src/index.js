bfd5956e373662f5e3ff317fde310d151d03c6c7
SergeyYakushev <yakushev563@gmail.com>
2020-11-18 10:01:40 +0300

    Финальный

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
+
+
+
 //import Tmpl from "./templates/example.hbs";
 //import cards from './example.json';
 import './styles.css';
 
-console.log("Hello11111");
+
+
+const refs = {
+    startBtn: document.querySelector('button[data-action="start"'),
+    stopBtn: document.querySelector('button[data-action="stop"'),
+}
+  
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+  const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+ const setRandomColor = () => {
+  const currentColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  console.log(currentColor);
+  document.body.style.backgroundColor = currentColor;
+}
+
+    let interval = undefined;
+
+refs.startBtn.addEventListener('click',onStartBtnClick );
+
+
+function onStartBtnClick() {
+  
+  return interval = interval ? interval : setInterval(() => setRandomColor(), 1000);
+
+}
+
+
+refs.stopBtn.addEventListener('click', () => clearInterval(interval));
